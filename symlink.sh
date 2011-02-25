18a71b51f2f14cb52c87c040d61c408386286513
Marko Seppä <marko.seppa@gmail.com>
2011-02-25 10:44:21 +0100

    symlink script

---
+++ symlink.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ln -nfs ~/.vim/zshrc ~/.zshrc
+ln -nfs ~/.vim/vimrc ~/.vimrc
+ln -nfs ~/.vim/gvimrc ~/.gvimrc
+ln -nfs ~/.vim/git/git-flow-completion/git-flow-completion.zsh ~/.git-flow-completion.zsh
