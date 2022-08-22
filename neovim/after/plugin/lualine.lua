f64cf8aba95d14b77851b6c54685b91922afb543
Marko Seppä <marko.seppa@gmail.com>
2022-08-22 10:59:48 +0200

    Configure lualine with relative path etc

--- neovim/after/plugin/lualine.lua
+++ neovim/after/plugin/lualine.lua
@@ -3,4 +3,17 @@ if not status_ok then
 	return
 end
 
-lualine.setup()
+lualine.setup({
+  sections = {
+    lualine_a = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+    },
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_x = {'filetype'},
+    lualine_c = {},
+  }
+})
