48a2135d7d66fba95bcada6ad16a749d6f4d1323
Marko Seppä <marko.seppa@gmail.com>
2022-08-22 08:28:20 +0200

    Fallback color

--- neovim/after/plugin/color.lua
+++ neovim/after/plugin/color.lua
@@ -2,4 +2,11 @@ vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_transparent = true
 vim.opt.background = "dark"
 
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd [[
+try
+  colorscheme tokyonight
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
