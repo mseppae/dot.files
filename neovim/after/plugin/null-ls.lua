674dc2b724b6ddc15d03c40847f81caaa79e1c2f
Marko Seppä <marko.seppa@gmail.com>
2023-02-07 21:54:20 +0100

    Add conditional rubocop diagnostics

--- neovim/after/plugin/null-ls.lua
+++ neovim/after/plugin/null-ls.lua
@@ -3,8 +3,20 @@ if not status_ok then
 	return
 end
 
+local utils = require("null-ls.utils").make_conditional_utils()
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+
+local rubocop_diagnostics = function()
+	if utils.root_has_file("Gemfile") then
+		return diagnostics.rubocop.with({
+			command = "bundle",
+			args = vim.list_extend({ "exec", "rubocop" }, diagnostics.rubocop._opts.args),
+		})
+	else
+		return diagnostics.rubocop
+	end
+end
 
 null_ls.setup({
 	debug = false,
@@ -13,7 +25,8 @@ null_ls.setup({
 		formatting.eslint_d,
 		formatting.stylua,
 		formatting.goimports,
+		formatting.rubocop,
 
-		diagnostics.rubocop,
+		rubocop_diagnostics(),
 	},
 })
