327fc92cf04afa199bf3259d303a32261651cced
Marko Seppä <marko.seppa@gmail.com>
2024-03-26 15:08:20 +0100

    Remove need to yank first on search replace in buffer

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,11 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map(
-  "n",
-  "<leader>cw",
-  '<Esc>:%s/<c-r>"//gc<Left><Left><Left>',
-  { desc = 'Confirm replace " register with new content' }
-)
+map("n", "<leader>cww", 'yiw<Esc>:%s/<c-r>"//gc<Left><Left><Left>', { desc = "Confirm replace nearest word" })
+map("n", "<leader>cw'", "yi'<Esc>:%s/<c-r>\"//gc<Left><Left><Left>", { desc = "Confirm replace nearest '" })
+map("n", '<leader>cw"', 'yi"<Esc>:%s/<c-r>"//gc<Left><Left><Left>', { desc = 'Confirm replace nearest "' })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
