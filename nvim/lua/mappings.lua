c8764b37d53ac47c03b2603095ad8efe2542df0a
Marko Seppä <marko.seppa@gmail.com>
2024-03-26 14:53:26 +0100

    Add "rename in buffer" mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,5 +6,11 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map(
+  "n",
+  "<leader>cw",
+  '<Esc>:%s/<c-r>"//gc<Left><Left><Left>',
+  { desc = 'Confirm replace " register with new content' }
+)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
