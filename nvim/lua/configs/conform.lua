d6352e099c721e9e1e4271c25ad68d02f6e0e5ff
Marko Seppä <marko.seppa@gmail.com>
2024-05-20 22:03:48 +0200

    Move conform options to more sane feeling place

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,17 +1,2 @@
-local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    ruby = { "solargraph" },
-    go = { "goimports", "gofmt" },
-    css = { { "prettierd", "prettier" } },
-    html = { { "prettierd", "prettier" } },
-  },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
-}
-
-require("conform").setup(options)
+-- Deletion of the file causes problems, probably some caching thing?
+-- Not bothered to spend time on this right now
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -2,9 +2,21 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre' -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        ruby = { "solargraph" },
+        go = { "goimports", "gofmt" },
+        css = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+      },
+
+      -- format_on_save = {
+      --   -- These options will be passed to conform.format()
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
+    },
   },
 
   {
