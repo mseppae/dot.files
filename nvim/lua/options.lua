8a119f611560b28c960a71a913d411cb704cfdcc
Marko Seppä <marko.seppa@gmail.com>
2024-05-21 21:48:42 +0200

    Cleanup options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,15 +1,4 @@
 require "nvchad.options"
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
-
 local opt = vim.opt
 opt.scrolloff = 999
--- opt.relativenumber = true
--- opt.showtabline = 0
-
-local g = vim.g
--- Explicitly set leader
-g.mapleader = " "
