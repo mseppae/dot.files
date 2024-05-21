408df166fa04058a7d476da76535d9063f709021
Marko Seppä <marko.seppa@gmail.com>
2024-05-21 21:48:22 +0200

    Add yank flash from kickstarter

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -135,3 +135,14 @@ end)
 map("n", "<C-s>", function()
   harpoon:list():next()
 end)
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
