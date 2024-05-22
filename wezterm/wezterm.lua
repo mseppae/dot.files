71ebb006269345003d3c81e09a4cd78db174ad37
Marko Seppä <marko.seppa@gmail.com>
2024-05-22 20:40:48 +0200

    Set wallpaper transparency

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,18 +1,19 @@
 -- Default keys: https://wezfurlong.org/wezterm/config/default-keys.html
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- TODO: parallax fancynes:
 -- * https://wezfurlong.org/wezterm/config/lua/config/background.html
 -- * https://www.gameartguppy.com/product-category/game-art-background-sprites/
--- 
--- current wallpaper from https://github.com/ItsTerm3n4l/Wallpapers
-config.window_background_image = wezterm.home_dir .. "/.config/Wallpapers/Night.png"
+--
+
+-- NOTE: Wallpaper not committed, currently using https://wallhaven.cc/w/85x6jo
+config.window_background_image = wezterm.home_dir .. "/.config/Wallpapers/Wallpaper.jpg"
 config.window_background_image_hsb = {
-  brightness = 0.02,
-  hue = 1.0,
-  saturation = 0.2,
+	brightness = 0.02,
+	hue = 0.8,
+	saturation = 0.7,
 }
 
 return config
