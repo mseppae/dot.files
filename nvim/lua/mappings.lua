f900ae19ecc4b64950f69ca9fcdc1ed13a3d8897
Marko Seppä <marko.seppa@gmail.com>
2024-07-31 23:32:37 +0200

    Fix and organize some mappings, remove duplicates

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -29,9 +29,8 @@ map("n", "]d", vim.diagnostic.goto_next, { desc = "Lsp next diagnostic" })
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Lsp diagnostic loclist" })
 
 -- Comment
-map("n", "<leader>/", function()
-  require("Comment.api").toggle.linewise.current()
-end, { desc = "Comment Toggle" })
+map("n", "<leader>/", "gcc", { desc = "Comment Toggle", remap = true })
+map("v", "<leader>/", "gc", { desc = "Comment Toggle", remap = true })
 
 -- nvimtree
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
@@ -62,41 +61,38 @@ end, { desc = "Whichkey query lookup" })
 -- Transition to [S]earch from [F]ind
 -- See `:help telescope.builtin`
 local builtin = require "telescope.builtin"
-vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set("n", "<leader>st", "<cmd>Telescope themes<CR>", { desc = "[S]earch [T]hemes" })
-vim.keymap.set("n", "<leader>sn", "<cmd>TodoTelescope<CR>", { desc = "[S]earch [N]otes" })
-vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
--- Slightly advanced example of overriding default behavior and theme
-vim.keymap.set("n", "<leader>/", function()
-  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = "[/] Fuzzily search in current buffer" })
-
--- add yours here
-
--- local map = vim.keymap.set
+local wk = require "which-key"
+wk.add {
+  { "<leader>s", group = "Search" },
+  { "<leader>sh", builtin.help_tags, desc = "[S]earch [H]elp" },
+  { "<leader>sk", builtin.keymaps, desc = "[S]earch [K]eymaps" },
+  { "<leader>sf", builtin.find_files, desc = "[S]earch [F]iles" },
+  { "<leader>ss", builtin.builtin, desc = "[S]earch [S]elect Telescope" },
+  { "<leader>sw", builtin.grep_string, desc = "[S]earch current [W]ord" },
+  { "<leader>sg", builtin.live_grep, desc = "[S]earch by [G]rep" },
+  { "<leader>sd", builtin.diagnostics, desc = "[S]earch [D]iagnostics" },
+  { "<leader>sr", builtin.resume, desc = "[S]earch [R]esume" },
+  { "<leader>s.", builtin.oldfiles, desc = '[S]earch Recent Files ("." for repeat)' },
+  { "<leader>st", "<cmd>Telescope themes<CR>", desc = "[S]earch [T]hemes" },
+  { "<leader>sn", "<cmd>TodoTelescope<CR>", desc = "[S]earch [N]otes" },
+  { "<leader><leader>", builtin.buffers, desc = "[ ] Find existing buffers" },
+  {
+    "<leader>s/",
+    function()
+      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end,
+    desc = "[/] Fuzzily search in current buffer",
+  },
+}
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>cww", 'yiw<Esc>:%s/<c-r>"//gc<Left><Left><Left>', { desc = "Confirm replace nearest word" })
 map("n", "<leader>cw'", "yi'<Esc>:%s/<c-r>\"//gc<Left><Left><Left>", { desc = "Confirm replace nearest '" })
 map("n", '<leader>cw"', 'yi"<Esc>:%s/<c-r>"//gc<Left><Left><Left>', { desc = 'Confirm replace nearest "' })
-
--- Telescope
-map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor" })
-map("n", "<leader>f<CR>", "<cmd>Telescope resume<CR>", { desc = "Resume last search" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
