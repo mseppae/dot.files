b67ddc1b4d8cf50b72404846fe24d70bc5be459a
Marko Seppä <marko.seppa@gmail.com>
2024-08-01 07:45:45 +0200

    Seems like the plugin is not sometimes yet loaded

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -61,32 +61,54 @@ end, { desc = "Whichkey query lookup" })
 -- Transition to [S]earch from [F]ind
 -- See `:help telescope.builtin`
 local builtin = require "telescope.builtin"
-local wk = require "which-key"
-wk.add {
-  { "<leader>s", group = "Search" },
-  { "<leader>sh", builtin.help_tags, desc = "[S]earch [H]elp" },
-  { "<leader>sk", builtin.keymaps, desc = "[S]earch [K]eymaps" },
-  { "<leader>sf", builtin.find_files, desc = "[S]earch [F]iles" },
-  { "<leader>ss", builtin.builtin, desc = "[S]earch [S]elect Telescope" },
-  { "<leader>sw", builtin.grep_string, desc = "[S]earch current [W]ord" },
-  { "<leader>sg", builtin.live_grep, desc = "[S]earch by [G]rep" },
-  { "<leader>sd", builtin.diagnostics, desc = "[S]earch [D]iagnostics" },
-  { "<leader>sr", builtin.resume, desc = "[S]earch [R]esume" },
-  { "<leader>s.", builtin.oldfiles, desc = '[S]earch Recent Files ("." for repeat)' },
-  { "<leader>st", "<cmd>Telescope themes<CR>", desc = "[S]earch [T]hemes" },
-  { "<leader>sn", "<cmd>TodoTelescope<CR>", desc = "[S]earch [N]otes" },
-  { "<leader><leader>", builtin.buffers, desc = "[ ] Find existing buffers" },
-  {
-    "<leader>s/",
-    function()
-      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end,
-    desc = "[/] Fuzzily search in current buffer",
-  },
-}
+-- local wk = require "which-key"
+-- wk.add {
+--   { "<leader>s", group = "Search" },
+--   { "<leader>sh", builtin.help_tags, desc = "[S]earch [H]elp" },
+--   { "<leader>sk", builtin.keymaps, desc = "[S]earch [K]eymaps" },
+--   { "<leader>sf", builtin.find_files, desc = "[S]earch [F]iles" },
+--   { "<leader>ss", builtin.builtin, desc = "[S]earch [S]elect Telescope" },
+--   { "<leader>sw", builtin.grep_string, desc = "[S]earch current [W]ord" },
+--   { "<leader>sg", builtin.live_grep, desc = "[S]earch by [G]rep" },
+--   { "<leader>sd", builtin.diagnostics, desc = "[S]earch [D]iagnostics" },
+--   { "<leader>sr", builtin.resume, desc = "[S]earch [R]esume" },
+--   { "<leader>s.", builtin.oldfiles, desc = '[S]earch Recent Files ("." for repeat)' },
+--   { "<leader>st", "<cmd>Telescope themes<CR>", desc = "[S]earch [T]hemes" },
+--   { "<leader>sn", "<cmd>TodoTelescope<CR>", desc = "[S]earch [N]otes" },
+--   { "<leader><leader>", builtin.buffers, desc = "[ ] Find existing buffers" },
+--   {
+--     "<leader>s/",
+--     function()
+--       builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+--         winblend = 10,
+--         previewer = false,
+--       })
+--     end,
+--     desc = "[/] Fuzzily search in current buffer",
+--   },
+-- }
+
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set("n", "<leader>st", "<cmd>Telescope themes<CR>", { desc = "[S]earch [T]hemes" })
+vim.keymap.set("n", "<leader>sn", "<cmd>TodoTelescope<CR>", { desc = "[S]earch [N]otes" })
+vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+-- Slightly advanced example of overriding default behavior and theme
+vim.keymap.set("n", "<leader>s/", function()
+  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = "[/] Fuzzily search in current buffer" })
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
