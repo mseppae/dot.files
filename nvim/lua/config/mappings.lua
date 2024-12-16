ed97cfd50d7f9b7d6617760a452c1f05e2345d11
Marko Seppae <marko.seppa@gmail.com>
2024-12-16 23:28:18 +0100

    Add quickfix next / prev binds

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -17,4 +17,7 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
 vim.keymap.set("n", "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
