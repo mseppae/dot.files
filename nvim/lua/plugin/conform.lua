70f4249a6311ded8fdf7b0c2e0a1379813b0a50c
Marko Seppä <marko.seppa@gmail.com>
2025-12-02 11:25:29 +0100

    Fix broken conform.nvim configuration

--- nvim/lua/plugin/conform.lua
+++ nvim/lua/plugin/conform.lua
@@ -21,38 +21,36 @@ vim.keymap.set(
 )
 
 require('conform').setup({
-  {
-    notify_on_error = true,
-    format_on_save = function(bufnr)
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = "never"
-      else
-        lsp_format_opt = "fallback"
-      end
-      return {
-        timeout_ms = 1500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
-    formatters_by_ft = {
-      lua = { "stylua" },
-      json = { "prettierd" },
-      ruby = { "rubocop" },
-      zig = { "zig fmt" },
-      go = {
-        "goimports-reviser",
-        "gofumpt",
-      },
-      css = { "prettierd" },
-      html = { "prettierd" },
-      javascript = { "prettierd" },
-    },
-    formatters = {
-      ["goimports-reviser"] = {
-        args = { "-d" },
-      },
+  notify_on_error = true,
+  format_on_save = function(bufnr)
+    local disable_filetypes = { c = true, cpp = true }
+    local lsp_format_opt
+    if disable_filetypes[vim.bo[bufnr].filetype] then
+      lsp_format_opt = "never"
+    else
+      lsp_format_opt = "fallback"
+    end
+    return {
+      timeout_ms = 1500,
+      lsp_format = lsp_format_opt,
+    }
+  end,
+
+  formatters_by_ft = {
+    lua        = { "stylua" },
+    ruby       = { "rubocop" },
+    zig        = { "zigfmt" },
+    go         = { "goimports-reviser", "gofumpt" },
+    json       = { "prettierd" },
+    css        = { "prettierd" },
+    html       = { "prettierd" },
+    javascript = { "prettierd" },
+    typescript = { "prettierd" },
+  },
+
+  formatters = {
+    ["goimports-reviser"] = {
+      prepend_args = { "-rm-unused", "-format" },
     },
   },
 })
